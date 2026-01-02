5b4e799ae9f2a0afb1fcae25d8df017e8870fc10
Shahzaib-Nawaz46 <shahzaibkhan9k@gmail.com>
2026-01-01 23:30:36 -0800

    testing for production

--- src/app/(Frontend)/User/Map/page.js
+++ src/app/(Frontend)/User/Map/page.js
@@ -4,14 +4,15 @@ import { MapContainer, TileLayer } from "react-leaflet";
 
 export default function SimpleMap() {
   return (
-    <MapContainer
-      center={[24.8607, 67.0011]} // Karachi
-      zoom={13}
-    >
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution="© OpenStreetMap contributors"
-      />
-    </MapContainer>
+    // <MapContainer
+    //   center={[24.8607, 67.0011]} // Karachi
+    //   zoom={13}
+    // >
+    //   <TileLayer
+    //     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    //     attribution="© OpenStreetMap contributors"
+    //   />
+    // </MapContainer>
+    <h1>Tesing</h1>
   );
 }
